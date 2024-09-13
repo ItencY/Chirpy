b81b10be1355216ee8ad5210f4bd4be501081dfc
itency <itency@outlook.com>
2024-09-13 13:46:13 +0300

    loading logo

--- main.go
+++ main.go
@@ -8,15 +8,17 @@ import (
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	const logofilepath = "/assets"
 
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/assets", http.FileServer(http.Dir(logofilepath)))
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Printf("Serving files from %s %s on port: %s\n", logofilepath, filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
