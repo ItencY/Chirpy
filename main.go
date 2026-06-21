5bb6a72bf1f17a895792c6b10beab39aa80bf0ed
ItencY <itency@outlook.com>
2026-06-21 09:33:33 +0300

    feat: add request metrics tracking and reset endpoints
    - Created apiConfig struct with atomic.Int32 to track file server hits
      safely across goroutines.
    - Implemented middlewareMetricsInc middleware to increment metrics on
      homepage/app requests.
    - Added /api/metrics endpoint to display current hits as plain text.
    - Added /api/reset endpoint to reset the hits counter to 0.

--- main.go
+++ main.go
@@ -3,22 +3,42 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	cfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+	mux.HandleFunc("/metrics", cfg.handlerMetrics)
+	mux.HandleFunc("/reset", cfg.handlerReset)
+
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
 	}
+
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
 }
