faab209badf985535a812cd8ea2869215213e001
ItencY <itency@outlook.com>
2026-06-21 10:01:33 +0300

    feat: restrict endpoints to specific HTTP methods using Go 1.22+ routing
    - Updated /healthz to accept only GET requests
    - Updated /metrics to accept only GET requests
    - Updated /reset to accept only POST requests
    - Enforced automatic 405 Method Not Allowed responses for other HTTP
      methods

--- main.go
+++ main.go
@@ -10,13 +10,6 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
@@ -26,13 +19,13 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
-	mux.HandleFunc("/metrics", cfg.handlerMetrics)
-	mux.HandleFunc("/reset", cfg.handlerReset)
+	mux.HandleFunc("GET /metrics", cfg.handlerMetrics)
+	mux.HandleFunc("POST /reset", cfg.handlerReset)
 
 	server := &http.Server{
 		Handler: mux,
