6fb5bdd936d90e0efcb6bcabc7b1fa58f8973666
ItencY <itency@outlook.com>
2026-06-25 08:09:16 +0300

    docs: update database reset response message
    Update the text to clearly state that hits and database are reset to
    their initial state.

--- reset.go
+++ reset.go
@@ -17,5 +17,5 @@ func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	}
 	cfg.fileserverHits.Store(0)
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0"))
+	w.Write([]byte("Hits reset to 0 and database reset to initial state."))
 }
